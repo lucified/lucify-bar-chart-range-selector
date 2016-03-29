ca1657dd2acdeb183ee29d22a3b811b33a391119
Ville Saarinen <ville.saarinen@gmail.com>
2016-03-29 14:59:15 +0300

    Use Babel and lucify-component-builder instead of webpack
    Still a WIP

---
+++ index.js
@@ -0,0 +1,14 @@
+import BarChartRangeSelector from './src/js/bar-chart-range-selector.jsx';
+import React from 'React';
+
+
+module.exports = function() {
+  var data = [ // TODO
+    {
+      key: ''
+    }
+  ];
+
+  return React.createElement(BarChartRangeSelector);
+};
+
